1ba8134c0b49c3a475c0453e7aaa47e339ed0565
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-09-26 15:41:38 +0100

    jdjdj

---
+++ test.js
@@ -0,0 +1,43 @@
+const data = ['Ade', 'Shina', 'Emma'];
+
+
+// OBJECT CLASS 
+const emmanuelData = {
+    name: 'Emmanuel',
+    age: 35,
+    department: 'Physiology',
+    level: 200,
+};
+
+const favourData = {
+    name: 'Favour',
+    age: 35,
+    department: 'Computer Science',
+    level: 400,
+};
+
+// console.log(emmanuelData.name)
+
+const cephasStudent = [
+    {
+        id: 1,
+        name: 'Emmanuel',
+        age: 35,
+        department: 'Physiology',
+        level: 200,
+    }, {
+        id: 2,
+        name: 'Favour',
+        age: 35,
+        department: 'Computer Science',
+        level: 400,
+    }, {
+        id: 3,
+        name: 'Ade',
+        age: 35,
+        department: 'Mathematics',
+        level: 300,
+    },
+]
+
+// console.log(cephasStudent)
