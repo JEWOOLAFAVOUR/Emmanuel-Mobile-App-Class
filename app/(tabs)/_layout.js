2ed7fe22179b1b6cc2bbaee2fcaa9cdcc4c49a70
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-09-30 16:15:51 +0100

    cool onces

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,20 +1,39 @@
 import { StyleSheet, Text, View, TextInput } from "react-native";
 import React from "react";
 
+
+const FormInput = ({ placeholder, kType }) => {
+  return (
+    <View style={styles.textCtn}>
+      <TextInput
+        placeholder={placeholder}
+        placeholderTextColor={"gray"}
+        keyboardType={kType}
+        secureTextEntry={true}
+        style={{ fontSize: 14, color: 'black' }}
+      />
+    </View>
+  )
+}
+
 const _layout = () => {
+
+
+
+
   return (
     <View style={styles.page}>
       <Text style={{ fontSize: 30, color: "black", fontWeight: "bold" }}>Input Class</Text>
 
-      <View style={styles.textCtn}>
-        <TextInput
-          placeholder="enter your name"
-          placeholderTextColor={"gray"}
-          keyboardType="numeric"
-          secureTextEntry={true}
-          style={{ fontSize: 14, color: 'black' }}
-        />
-      </View>
+      <FormInput placeholder={"enter your name"} />
+      <FormInput placeholder={"enter your email"} kType={"numeric"} />
+      <FormInput />
+      <FormInput />
+      <FormInput />
+      <FormInput />
+      <FormInput />
+      <FormInput />
+
 
     </View>
   );
